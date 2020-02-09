1e5f86e6e2034359b16cd3d96f740ee724884691
aymendirar <m5y2b@ugrad.cs.ubc.ca>
2020-02-09 12:17:25 -0800

    Fixed a typo in Main

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -1,6 +1,6 @@
 package ui;
 
-// Intializes the Get It Done! application
+// Initializes the Get It Done! application
 public class Main {
     public static void main(String[] args) {
         new GetItDoneApp();
